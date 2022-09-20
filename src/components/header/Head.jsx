4f52383934e6c26e5ca6cb9d7814dfb633f4d928
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-09-20 17:55:09 +0330

    start shopping website

---
+++ src/components/header/Head.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FiPhone, FiMail } from "react-icons/fi";
+const Head = () => {
+  return (
+    <>
+<section className='head'>
+<div className='container d-flex'>
+<div className='left row'>
+<FiPhone/>
+<label> 09140000000</label>
+<FiMail/>
+<label>jamal.shamsi@gmail.com</label>
+</div>
+<div className='right row RText'>
+    <labe>Theme FAQ's </labe>
+    <label>Need Helps</label>
+    <span></span>
+    
+</div>
+
+</div>
+
+
+
+
+</section>
+    </>
+  )
+}
+
+export default Head
